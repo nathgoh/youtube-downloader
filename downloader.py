9353b89aab78321428eb9d0bfe1b850f0d6e7bcc
Nathaniel Goh <gohnathaniel2000@gmail.com>
2024-09-07 16:45:43 -0700

    Add some metadata

--- downloader.py
+++ downloader.py
@@ -1,10 +1,30 @@
+from io import BytesIO
+from PIL import Image
 from pytube import Playlist
+import os
+import requests
 import pytube as yt
+import music_tag
+
 
 playlist = Playlist("https://www.youtube.com/playlist?list=PLYHI_9Wc3IgkzmYCufwGBiHh2lSOBcSPR")
 
 download_folder = "./downloads"
 
 for video in playlist.videos:
+    thumbnail = video.thumbnail_url
     title = video.title
-    video.streams.filter(only_audio=True).first().download(output_path=download_folder, filename=title)
+    artist = video.author
+    file = f"{download_folder}/{title}.mp3"
+    
+    if not os.path.isfile(file):
+        response = requests.get(thumbnail)
+        image = Image.open(BytesIO(response.content))
+        image.convert('RGB').save(f"{download_folder}/thumbnail.jpeg", 'JPEG')
+        video.streams.get_audio_only().download(output_path=download_folder, filename=f"{title}.mp3")
+        tagging = music_tag.load_file(file)
+        tagging["artist"] = artist
+        
+        with open(f"{download_folder}/thumbnail.jpeg", "rb") as img: 
+            tagging["artwork"] = img.read()
+        tagging.save()
